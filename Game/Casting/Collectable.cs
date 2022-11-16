f250af41b5c11071f2749f4eb3c5b1129d761261
ReArquitecto <94197258+ReArquitecto@users.noreply.github.com>
2022-11-16 09:23:48 -0700

    kill everything

---
+++ Game/Casting/Collectable.cs
@@ -0,0 +1,11 @@
+namespace CSE210_04.Game.Casting
+{
+    public class Collectable : Actor
+    {
+        public Collectable(Game game, Vector2 position, string spriteName)
+            : base(game, position, spriteName);
+
+            
+
+    }
+}
