31266be444298d6d435d62781a84a85eec1fd33e
ReArquitecto <94197258+ReArquitecto@users.noreply.github.com>
2022-11-16 09:29:31 -0700

    up to date

--- Game/Casting/Collectable.cs
+++ Game/Casting/Collectable.cs
@@ -2,10 +2,22 @@ namespace CSE210_04.Game.Casting
 {
     public class Collectable : Actor
     {
+        private int _points;
+
         public Collectable(Game game, Vector2 position, string spriteName)
             : base(game, position, spriteName);
 
-            
+        public int GetPoints()
+        {
+            return _points;
+        }
+
+        public void SetPoints(int points)
+        {
+            _points = points;
+        }
+
+
 
     }
 }
